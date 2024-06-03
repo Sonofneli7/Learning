3e6217bcab8076f1595069e801d899d3dddf74e2
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2024-06-03 08:43:58 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ApprenticeChecklist {
+	requires java.desktop;							//includes APIs: packages such as javax.swing (for Swing GUI components), java.awt (Abstract Window Toolkit), and java.applet
+}
